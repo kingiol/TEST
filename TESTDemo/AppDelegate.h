a1f6fae86a133d83649e5f546b509dabdfa19ed7
Kingiol <kingiol@KingioltekiMacBook-Pro.local>
2013-03-15 20:34:13 +0800

    Add TestDemo

---
+++ TESTDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TESTDemo
+//
+//  Created by Kingiol on 13-3-15.
+//  Copyright (c) 2013年 Kingiol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TESTDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TESTDemo
+//
+//  Created by Kingiol on 13-3-15.
+//  Copyright (c) 2013年 Kingiol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
